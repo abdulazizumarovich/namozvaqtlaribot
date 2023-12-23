0c6a03cc8405b33091d43d0ecc63a784a80669fc
abdulaziz <abdulazizumarovich@gmail.com>
2023-12-23 13:33:50 +0500

    express for heroku web listening

--- index.js
+++ index.js
@@ -1,45 +1,67 @@
-const { Telegraf } = require('telegraf');
-const schedule = require('node-schedule');
-const dotenv = require('dotenv');
-const data = require('./data.js')
-const settings = require('./reminder_settings.json');
+const { Telegraf } = require('telegraf')
+const schedule = require('node-schedule')
+const express = require('express')
+require('dotenv').config()
 
-dotenv.config();
-const bot = new Telegraf(process.env.BOT_TOKEN);
-const chatID = process.env.CHAT_ID;
+const data = require('./data.js')
+const settings = require('./reminder_settings.json')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const chatID = process.env.CHAT_ID
+const port = process.env.PORT || 5500
+
+/**
+ * Just for heroku to continue web app's running state
+ */
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Kirgan kirdi telegram bot ishlayapti.'});
+});
+// TELEGRAM WEBHOOK - https://core.telegram.org/bots/api#setwebhook
+app.post(`/${process.env.BOT_TOKEN}`, (req, res) => {
+	bot.processUpdate(req.body);
+	res.status(200).json({ message: 'ok' });
+});
+
+app.listen(port, () => {
+	console.log(`\n\nKirgan kirdi web serveri. port: ${port}.\n\n`);
+});
+//heroku end
 
 let currentData = {}
 
 function createReminderObjects(todayData) {
-  const dateToday = new Date();
-  dateToday.setSeconds(0);
-  dateToday.setMilliseconds(0);
+  const dateToday = new Date()
+  dateToday.setSeconds(0)
+  dateToday.setMilliseconds(0)
   return Object.entries(todayData)
     .filter(([key]) => key != 'sana')
     .map(([key, time]) => {
-      const [hours, minutes] = time.split(':').map(Number);
-      const date = new Date(dateToday);
-      date.setHours(hours);
-      date.setMinutes(minutes);
-      return { key, date };
-    });
+      const [hours, minutes] = time.split(':').map(Number)
+      const date = new Date(dateToday)
+      date.setHours(hours)
+      date.setMinutes(minutes)
+      return { key, date }
+    })
 }
 
 function sendTimeAlerts(todayData) {
   if (!todayData || todayData.length === 0) {
     console.log('No data')
-    bot.telegram.sendMessage(chatID, 'Ey, loglarga qara!');
-    return;
+    bot.telegram.sendMessage(chatID, 'Ey, loglarga qara!')
+    return
   }
 
   bot.telegram.sendMessage(chatID, prettyInfo())
 
-  const now = new Date();
+  const now = new Date()
   todayData.forEach(({ date, key }) => {
     const alert = settings.alerts.find(alert => alert.key === key)
     const alertType = settings.alert_types.find(alert_type => alert_type.type == alert.alert_type)
     alertType.reminders_before.forEach(reminder => {
-      const reminderDate = new Date(date.getTime() - reminder * 60 * 1000);
+      const reminderDate = new Date(date.getTime() - reminder * 60 * 1000)
       if (reminderDate > now) {
         schedule.scheduleJob(reminderDate, () => {
           let reminderMessage = ''
@@ -48,18 +70,18 @@ function sendTimeAlerts(todayData) {
             if (key == 'quyosh')
               reminderMessage = 'Quyosh chiqdiyov oynadan qarachi'
             else
-              reminderMessage = `Kirgan kirdi. ${alert.name} vaqti bo'ldi`;
+              reminderMessage = `Kirgan kirdi. ${alert.name} vaqti bo'ldi`
           } else {
-            reminderMessage = `${alert.name}ga ${reminder} daqiqa qoldi`;
+            reminderMessage = `${alert.name}ga ${reminder} daqiqa qoldi`
             if (key != 'quyosh')
               reminderMessage = '🍇 Uzumlar chayilganmi? ' + reminderMessage
           }
 
-          bot.telegram.sendMessage(chatID, reminderMessage);
-        });
+          bot.telegram.sendMessage(chatID, reminderMessage)
+        })
       }
-    });
-  });
+    })
+  })
 }
 
 function prettyInfo() {
@@ -77,9 +99,9 @@ function prettyInfo() {
 async function start() {
   try {
     console.log('Setting scheduler for today\'s reminders')
-    currentData = await data.today();
-    const timeDates = createReminderObjects(currentData);
-    sendTimeAlerts(timeDates);
+    currentData = await data.today()
+    const timeDates = createReminderObjects(currentData)
+    sendTimeAlerts(timeDates)
     console.log('Scheduler is set')
   } catch (e) {
     console.log(e)
@@ -87,12 +109,12 @@ async function start() {
   }
 }
 
-schedule.scheduleJob('0 0 9 * * *', () => start());
+schedule.scheduleJob('0 0 9 * * *', () => start())
 
 bot.start((ctx) => {
   ctx.reply(prettyInfo())
-});
+})
 
-bot.launch();
+bot.launch()
 
 start()
